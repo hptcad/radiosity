0bb9a03f139ed04b8b66c050336448a7577bbec8
manstetten <manstetten@iue.tuwien.ac.at>
2016-01-13 12:07:21 +0100

    changed venv

--- radiosity/odradio_tests/main.py
+++ radiosity/odradio_tests/main.py
@@ -1,5 +1,5 @@
 import odradio as od
 
-#od.cylinder.cylidner(diameter=500,depth=2500,s=0.2)
-#od.cone.cone(diameter=500,depth=2500,s=0.2,rfactor=0.5)
+od.cylinder.cylidner(diameter=500,depth=2500,s=0.2)
+od.cone.cone(diameter=500,depth=2500,s=0.2,rfactor=0.5)
 od.concave.concave(diameter=500,depth=2500,s=0.2,rfactor=2,zfactor=0.5)
